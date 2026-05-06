c906f7100d7851047c4698d04c35cd3df4218707
George Chitu <doru.chitu96@gmail.com>
2026-05-06 10:02:16 +0300

    structure file for cypress

---
+++ frontend/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    setupNodeEvents(on, config) {},
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
+  },
+})
