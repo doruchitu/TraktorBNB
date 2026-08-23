eabd3f25ea48f2a59565e3c6ec8749967694f348
George Chitu <doru.chitu96@gmail.com>
2026-08-23 15:35:21 +0300

    final fix for PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       manifest: {
         name: 'TraktorBNB',
         short_name: 'TraktorBNB',
