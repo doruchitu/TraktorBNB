d4873dc3ea3f478a4a5cb69d32c13b17cbf3f9a9
George Chitu <doru.chitu96@gmail.com>
2026-09-07 10:44:56 +0300

    minor edit

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sfiqab5ifjo"
+    "revision": "0.5p2hjm7sm3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
